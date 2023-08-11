a1940bc67cb30967f289fc1cabf22998075ff3ec
Agustin Rolando <138535452+Agustin144@users.noreply.github.com>
2023-08-11 17:27:38 +0000

    segundo commit, falta pasar de la primera a la ultima para atras

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,10 +7,11 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 
-	
 
 
-	const [indexSong, setindexSong] = useState(null)
+	// const [position, setPosition] = useState(0)
+
+	const [indexSong, setindexSong] = useState(0)
 	const audioRef = useRef(null)
 	const [isPlaying, setIsPlaying] = useState(false)
 	const [songs, setSongs] = useState([])
@@ -25,22 +26,65 @@ const Home = () => {
 
 
 	}
-	console.log(songs);
+	// console.log(songs);
 
 	useEffect(() => {
 		getSongsData();
 	}, []);
-	const play=() =>{
+	const play = () => {
 		if (isPlaying) {
 			audioRef.current.pause()
-			
-		}else{
+
+		} else {
 			audioRef.current.play()
 		}
 
-console.log(songURL + indexSong);
+		console.log(songURL + indexSong);
 		setIsPlaying(!isPlaying)
 	}
+	const start2 =(id)=>{
+		// 
+		setIsPlaying(true)
+		setindexSong(id)
+		songs.filter(song=>{
+			if (song.id==id) {
+			audioRef.current.src = 	"https://playground.4geeks.com/apis/fake/sound/" + song.url
+			audioRef.current.play()
+			console.log(indexSong);
+			}
+			
+		})
+	}
+//boton para atras
+	const back=()=> {
+	
+		songs.filter(song=>{
+			if (song.id==indexSong && song.id!==0) {
+
+			audioRef.current.src = 	"https://playground.4geeks.com/apis/fake/sound/" + songs[indexSong -1].url;
+			setindexSong(indexSong -1)
+			audioRef.current.play()
+			console.log(audioRef.current,);
+			}
+		})
+	
+		
+	}
+	const next=()=>{
+		songs.filter(song=>
+			
+			{if (song.id==indexSong) {
+
+			audioRef.current.src = 	"https://playground.4geeks.com/apis/fake/sound/" + songs[indexSong +1].url;
+			setindexSong(indexSong +1)
+			audioRef.current.play()
+			console.log(audioRef.current,);
+			}
+			
+		})
+
+	}
+
 
 	return (
 
@@ -48,25 +92,25 @@ console.log(songURL + indexSong);
 			<div className="text-center list-group list-group-numbered ">
 				{songs.map((song, index) =>
 					<li className="list-group-item bg-dark nombres" key={song.id} onClick={() => {
-						setindexSong(song.url)
-						// play()
+						
+						start2(song.id)
 
 					}}>{song.name}</li>
 				)}
 			</div>
 			<ul className="nav justify-content-center barra">
 				<li className="nav-item1">
-					<a className="nav-link back"><i className="fa fa-square-caret-left"/></a>
+					<a className="nav-link back"><i className="fa fa-square-caret-left" onClick={back} /></a>
 				</li>
 				<li className="nav-item2">
-					<a className="nav-link play" ><i className={isPlaying ? "fa-solid fa-pause" : "fa-solid fa-play"}onClick={play}/></a>
+					<a className="nav-link play" ><i className={isPlaying ? "fa-solid fa-pause" : "fa-solid fa-play"} onClick={play} /></a>
 				</li>
 				<li className="nav-item3">
-					<a className="nav-link next" ><i className="fa fa-square-caret-right"/></a>
-				</li> 
+					<a className="nav-link next" ><i className="fa fa-square-caret-right" onClick={next}/></a>
+				</li>
 
 			</ul>
-			<audio ref={audioRef} src={songURL + indexSong} />
+			<audio ref={audioRef} src= " " />
 		</div>
 	);
 };
